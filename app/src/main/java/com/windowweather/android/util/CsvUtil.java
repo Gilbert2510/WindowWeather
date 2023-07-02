fae941a248e468b8f59f6dfb4062765ecfcf45f5
Gilbert2510 <1023161984@qq.com>
2023-07-03 01:21:27 +0800

    搜索功能实现

--- app/src/main/java/com/windowweather/android/util/CsvUtil.java
+++ app/src/main/java/com/windowweather/android/util/CsvUtil.java
@@ -1,14 +1,38 @@
 package com.windowweather.android.util;
 
+import android.content.Context;
 import android.os.Environment;
 
+import com.windowweather.android.db.CitySearch;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 
 public class CsvUtil {
-    private void readDataCsv() {
-        int i=0;
+    private void readDataCsv(Context context) {
+        InputStreamReader is=null;
         try {
-            File csv=new File(Environment.getExternalStorageDirectory()+)
+            is=new InputStreamReader(context.getAssets().open("China-City-List-latest.csv"));
+            BufferedReader reader=new BufferedReader(is);
+            reader.readLine();
+            String line="";
+            while((line=reader.readLine())!=null) {
+                String buffer[]=line.split(",");
+                CitySearch search=new CitySearch();
+                String name=buffer[2];
+                String adm2=buffer[9];
+                String adm1=buffer[7];
+                String country=buffer[5];
+                search.setCityId(buffer[0]);
+                search.setCityName(name);
+                search.setCityAdm2(adm2);
+                search.setCityAdm1(adm1);
+                search.setCityCountry(country);
+                search.setAreaName(name+"-"+adm2+"-"+adm1+"-"+country);
+                search.save();
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
